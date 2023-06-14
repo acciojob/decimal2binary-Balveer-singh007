e6d726e9aee603fdb4b3a947f2a267bf6bf8bc98
Balveer Singh <128572901+Balveer-singh007@users.noreply.github.com>
2023-06-14 16:05:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function threeSum(arr, target) {
 // write your code here
+	var diff = Number.MAX_VALUE;
+        var ans = 0;
+
+        for(let i = 0; i < arr.length; i++){
+            for(let j = i + 1; j < arr.length; j++){
+                for(let k = j + 1; k < arr.length; k++){
+                    let sum = arr[i]  + arr[j] + arr[k];
+
+                    if(Math.abs(sum - target) < diff){
+                        diff = Math.abs(sum - target);
+                        ans = sum;
+                    }
+                }
+            }
+        }
+        return ans;
   
 }
 
